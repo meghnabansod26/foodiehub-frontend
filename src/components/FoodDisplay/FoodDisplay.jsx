7725f98e193e320a88d17fcfd04db594935b9756
Meghna Bansod <meghnabansod26@gmail.com>
2026-02-13 20:49:50 +0530

    Added loader in FoodDisplay component

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,39 +1,51 @@
-import React from 'react';
-import { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from '../FoodItem/FoodItem';
 
-
-const FoodDisplay = ({category, searchText}) => {
+const FoodDisplay = ({ category, searchText }) => {
   const { foodList } = useContext(StoreContext);
-  const filteredFoods = foodList.filter(food =>(
-       ( category === 'All' || food.category === category) &&
-       food.name.toLowerCase().includes(searchText.toLowerCase())
-       ));
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (foodList && foodList.length > 0) {
+      setLoading(false);
+    }
+  }, [foodList]);
+
+  const filteredFoods = foodList.filter(food => (
+    (category === 'All' || food.category === category) &&
+    food.name.toLowerCase().includes(searchText.toLowerCase())
+  ));
+
   return (
     <div className="container">
       <div className="row">
-        {
-          filteredFoods.length > 0 ? (
-            filteredFoods.map((food, index) => (
-             <FoodItem key={index} 
-             name={food.name}
-             description={food.description}
-             id={food.id}
-             imageUrl={food.imageUrl}
-             price={food.price}
-             />
-            ))
 
-          ) : (
-            <div className='text-center mt-4'>
-              <h4>No food found.</h4>
-            </div>
-          )
-        }
+        {loading ? (
+          <div className="text-center mt-5 w-100">
+            <div className="spinner-border text-primary" role="status"></div>
+            <p className="mt-3">Starting server... Please wait</p>
+          </div>
+        ) : filteredFoods.length > 0 ? (
+          filteredFoods.map((food, index) => (
+            <FoodItem
+              key={index}
+              name={food.name}
+              description={food.description}
+              id={food.id}
+              imageUrl={food.imageUrl}
+              price={food.price}
+            />
+          ))
+        ) : (
+          <div className="text-center mt-4 w-100">
+            <h4>No food items found</h4>
+          </div>
+        )}
+
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
